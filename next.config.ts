ad6f1a31adedf30acd213769667a7841fa388bf8
Marcelo Vieira <marcelotiaju@gmail.com>
2025-10-22 22:01:21 -0300

    Ajustes Next Config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production' ? {exclude: ['error']} : false,
+  },
+  reactStrictMode: true, 
   typescript: {
      ignoreBuildErrors: true,
   },
        experimental: {
         cpus: 1,
-        webpackBuildWorker: true
+        webpackBuildWorker: true,
+        workerThreads: false,
     },
     transpilePackages: ['@acme/ui', 'lucide-react', 'react-number-format', 'react-hook-form', 'zod', 'axios', 'date-fns', 'react', 'react-dom', 'next',
       'mermaid', 'marked', '@tanstack/react-query', '@tanstack/query-core', '@tanstack/react-query-devtools', '@headlessui/react', '@radix-ui/react-dialog', '@radix-ui/react-tabs', '@radix-ui/react-checkbox', '@radix-ui/react-popover', '@radix-ui/react-select', '@radix-ui/react-dropdown-menu', '@radix-ui/react-tooltip', 'clsx', 'tailwind-merge', 'react-hot-toast'
