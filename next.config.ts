d253a7fbf1faee3cb290fd1e30ad86a6d2114663
MARCELO VIEIRA ALVES DE ANDRADE <marcelotiaju@gmail.com>
2025-12-17 23:27:11 -0300

    Ajustes config next

--- next.config.ts
+++ next.config.ts
@@ -8,16 +8,16 @@ const nextConfig: NextConfig = {
   typescript: {
      ignoreBuildErrors: true,
   },
-  /*   experimental: {
+     experimental: {
         cpus: 1,
         webpackBuildWorker: true,
         workerThreads: false,
-    },
+    },/*
     transpilePackages: ['@acme/ui', 'lucide-react', 'react-number-format', 'react-hook-form', 'zod', 'axios', 'date-fns', 'react', 'react-dom', 'next',
       'mermaid', 'marked', '@tanstack/react-query', '@tanstack/query-core', '@tanstack/react-query-devtools', '@headlessui/react', '@radix-ui/react-dialog', '@radix-ui/react-tabs', '@radix-ui/react-checkbox', '@radix-ui/react-popover', '@radix-ui/react-select', '@radix-ui/react-dropdown-menu', '@radix-ui/react-tooltip', 'clsx', 'tailwind-merge', 'react-hot-toast'
-    ],
+    ],*/
      staticPageGenerationTimeout: 1000,
-     bundlePagesRouterDependencies: true,*/
+     bundlePagesRouterDependencies: true,
 };
 
 export default nextConfig;
