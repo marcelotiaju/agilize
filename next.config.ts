e9ce30d3a2e533b6026d02d1e3e4bf19e874a379
MARCELO VIEIRA ALVES DE ANDRADE <marcelotiaju@gmail.com>
2025-12-19 17:57:30 -0300

    Ajustes next config

--- next.config.ts
+++ next.config.ts
@@ -9,19 +9,20 @@ const nextConfig: NextConfig = {
      ignoreBuildErrors: true,
   },
      experimental: {
-        cpus: 1,
-        webpackBuildWorker: true,
-        workerThreads: false,
+        optimizeCss: false,
+        //cpus: 1,
+        //webpackBuildWorker: true,
+        //workerThreads: false,
     },/*
     transpilePackages: ['@acme/ui', 'lucide-react', 'react-number-format', 'react-hook-form', 'zod', 'axios', 'date-fns', 'react', 'react-dom', 'next',
       'mermaid', 'marked', '@tanstack/react-query', '@tanstack/query-core', '@tanstack/react-query-devtools', '@headlessui/react', '@radix-ui/react-dialog', '@radix-ui/react-tabs', '@radix-ui/react-checkbox', '@radix-ui/react-popover', '@radix-ui/react-select', '@radix-ui/react-dropdown-menu', '@radix-ui/react-tooltip', 'clsx', 'tailwind-merge', 'react-hot-toast'
     ],*/
-     staticPageGenerationTimeout: 1000,
-     bundlePagesRouterDependencies: true,
-     outputFileTracingIncludes: {
-       'src/app/api/contributors/upload/route.ts': ['fs', 'path'],
-       'src/app/api/contributors/route.ts': ['fs', 'path'],
-       '/public/uploads': ['fs', 'path'],
+    //  staticPageGenerationTimeout: 1000,
+    //  bundlePagesRouterDependencies: true,
+    //  outputFileTracingIncludes: {
+    //    'src/app/api/contributors/upload/route.ts': ['fs', 'path'],
+    //    'src/app/api/contributors/route.ts': ['fs', 'path'],
+    //    '/public/uploads': ['fs', 'path'],
      },
 };
 
