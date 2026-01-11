dd3efb9f9c4923507fe16d2488f3748403d0f47d
MARCELO VIEIRA ALVES DE ANDRADE <marcelotiaju@gmail.com>
2026-01-10 22:56:42 -0300

    fix: Ajuste Logo 4

--- src/app/api/reports/contributors/route.ts
+++ src/app/api/reports/contributors/route.ts
@@ -9,7 +9,10 @@ import { zonedTimeToUtc, utcToZonedTime } from 'date-fns-tz'
 
 const fs = require('fs');
 
-const imageFile = fs.readFileSync('./Logo.png');
+const { resolve } = require('path');
+const filePath = resolve(__dirname, './Logo.png');   
+
+const imageFile = fs.readFileSync(filePath);
 const base64String = Buffer.from(imageFile).toString('base64');
 
 // Função auxiliar para formatar moeda
